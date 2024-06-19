6669884c29fc4db1a0fdac0dde603a8995c5136e
aplz1 <antonevtuxov@gmail.com>
2024-06-19 23:51:58 +0300

    Spring structure

---
+++ src/main/java/ru/studentsvsuet/airline/AirlineApplication.java
@@ -0,0 +1,13 @@
+package ru.studentsvsuet.airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/studentsvsuet/airline/AirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.studentsvsuet.airline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
